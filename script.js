8e474d319306684d2ebf3ff1f06ea0645b50d3c6
Rizwan462 <139914755+Rizwan462@users.noreply.github.com>
2024-09-07 22:52:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function(){
+	const form = document.getElementById('loginForm');
+	const usernameInput = document.getElementById('username');
+	const passwordInput = document.getElementById('password');
+	const remCheckbox = document.getElementById('checkbox');
+	const existUserbtn = document.getElementById('existing');
+
+	if(localStorage.getItem('username') && localStorage.getItem('password')){
+		existUserbtn.style.display = 'block';
+	}
+	form.addEventListener('submit', function (e){
+		e.preventDefault();
+		const username = usernameInput.value;
+		const password = passwordInput.value;
+
+		if(remCheckbox.checked) {
+			localStorage.setItem('username', username);
+			localStorage.setItem('password', password);
+		}else{
+			localStorage.removeItem('username');
+			localStorage.removeItem('password');
+		}
+		alert(`Logged in as ${username}`);
+	});
+
+	existUserbtn.addEventListener('click', function(ev){
+		ev.preventDefault();
+		const savedUsername = localStorage.getItem('username');
+		if(savedUsername){
+			alert(`Logged in as ${savedUsername}`);
+		}
+	});
+	
+});
